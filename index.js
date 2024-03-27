49771725ecbf48abe613f1855dfbd01f56ca28f2
vincent <vinnymummo@gmail.com>
2024-03-27 19:16:28 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+    return cats;
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats;
+}
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield")
+    return cats;
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo")
+    return cats;
+}
+function appendCat(name){
+    const catss = [...cats,name]
+    return catss;
+}
+function prependCat(name){
+    const catss = [name,...cats]
+    return catss;
+}
+function removeLastCat(){  
+const catss = cats.slice(0,-1)
+    return catss
+}
+function removeFirstCat(){  
+    const catss = cats.slice(1)
+        return catss
+    }
 // Write your solution here!
